70007197f90f15c6b63b6976a29791847e441502
yoloinfinity55 <yoloinfinity55@gmail.com>
2025-09-18 11:47:14 -0400

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,7 +3,7 @@ module.exports = {
   description: 'Eleventy starter project to create a blog',
   keywords: ['eleventy', 'template', 'simple', 'clean'],
   url: 'https://vredeburg.netlify.app', // your site url without trailing slash
-  paginate: 4 // how many posts you want to show for each page
+  paginate: 6 // how many posts you want to show for each page
   // uncomment the next line if you want to add disqus to your site
   // disqusShortname: "your-shortname"
 };
